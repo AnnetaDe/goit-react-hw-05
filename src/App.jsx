e10d2dab978ddfb9270d8aeb761afd4b9794adfb
AnnetaDe <anneta.liss@gmail.com>
2024-05-15 15:52:36 +0200

    13

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,8 @@
-// '/' – компонент HomePage, домашня сторінка із списком популярних кінофільмів.
-// '/movies' – компонент MoviesPage, сторінка пошуку кінофільмів за ключовим словом.
-// '/movies/:movieId' – компонент MovieDetailsPage, сторінка із детальною інформацією про кінофільм.
-// /movies/:movieId/cast – компонент MovieCast, інформація про акторський склад. Рендериться в нижній частині на сторінці MovieDetailsPage.
-// /movies/:movieId/reviews – компонент MovieReviews, інформація про огляди. Рендериться в нижній частині на сторінці MovieDetailsPage.
-// Якщо користувач зайшов за неіснуючим маршрутом, потрібно показувати компонент NotFoundPage, в якому є посилання Link на домашню сторінку.
-
 import { lazy, Suspense } from 'react';
 
 //
 import { Routes, Route } from 'react-router-dom';
 
-// import HomePage from './pages/HomePage.jsx';
 import NotFoundPage from './pages/NotFoundPage.jsx';
 import Navigation from './components/Navigation/Navigation.jsx';
 
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,17 +1,21 @@
 import { NavLink, Link } from 'react-router-dom';
 import s from './Navigation.module.css';
 import clsx from 'clsx';
+
+const buildLinkClass = ({ isActive }) => {
+  return clsx(s.link, isActive && s.active);
+};
 export const Navigation = () => {
   return (
     <header className={s.header}>
       <ul>
         <li>
-          <NavLink className={'activeLink'} to="/">
+          <NavLink className={buildLinkClass} to="/">
             Home
           </NavLink>
         </li>
         <li>
-          <NavLink className={'activeLink'} to="/movie_search">
+          <NavLink className={buildLinkClass} to="/movie_search">
             Movies
           </NavLink>
         </li>
